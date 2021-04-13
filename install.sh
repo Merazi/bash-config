79c69b5225b183b69964431efabe95ab54274041
Merazi <merazguerra@protonmail.com>
2021-04-13 01:55:43 -0600

    My configuration from Apr13, 2021

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+__origin=$(pwd)
+
+function __install_configuration_files()
+{
+    # this will make symbolic links to
+    # the files on the repository
+    ln -s ${__origin}/bashrc ~/.bashrc;
+    ln -s ${__origin}/aliases ~/.bash_aliases;
+    ln -s ${__origin}/functions ~/.bash_functions;
+    # if you delete the repostiory the configuration
+    # files will be gone
+}
+
+if [ -d .git ]; then
+    # we're on the git directory
+    echo "Installing the files...";
+    __install_configuration_files;
+else
+    echo "Plase run from the root of the cloned directory."
+fi
