894e20da1964cc22be416f67452cb31d6ad7f318
merazi <merazi@tuta.io>
2021-05-24 07:41:48 -0600

    Updated the installer script

--- install.sh
+++ install.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
-__origin=$(pwd)
+origin_dir=$(pwd)
 
-function __install_configuration_files()
+function install_configuration_files()
 {
     # this will make symbolic links to
     # the files on the repository
-    ln -s ${__origin}/bashrc ~/.bashrc;
-    ln -s ${__origin}/aliases ~/.bash_aliases;
-    ln -s ${__origin}/functions ~/.bash_functions;
+    ln -s ${origin_dir}/bashrc ~/.bashrc;
+    ln -s ${origin_dir}/aliases ~/.bash_aliases;
+    ln -s ${origin_dir}/functions ~/.bash_functions;
+    ln -s ${origin_dir}/profile ~/.bash_profile
     # if you delete the repostiory the configuration
     # files will be gone
 }
@@ -16,7 +17,7 @@ function __install_configuration_files()
 if [ -d .git ]; then
     # we're on the git directory
     echo "Installing the files...";
-    __install_configuration_files;
+    install_configuration_files;
 else
     echo "Plase run from the root of the cloned directory."
 fi
