8c163f5eba522863d57aeeb8e978a74297ff2ca9
Merazi <merazi_pub@riseup.net>
2021-10-04 21:35:14 -0600

    Now this refers to the new files.

--- install.sh
+++ install.sh
@@ -8,9 +8,8 @@ function install()
     # the files on the repository
 
     ln -s ${origin_dir}/bashrc ~/.bashrc;
-    ln -s ${origin_dir}/aliases ~/.bash_aliases;
-    ln -s ${origin_dir}/functions ~/.bash_functions;
-    ln -s ${origin_dir}/profile ~/.bash_profile;
+    ln -s ${origin_dir}/aliases ~/.aliases;
+    ln -s ${origin_dir}/bash_profile ~/.bash_profile;
 
     # if you delete the repostiory the configuration
     # files will be gone
